5652805c9cea06eb2e406166505189e09b97a706
Eitan Sela <eitan.sela@gmail.com>
2017-12-01 19:22:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# x-ray-spring-boot-app
---
+++ src/main/java/com/eitan/spring/xray/Application.java
@@ -0,0 +1,45 @@
+package com.eitan.spring.xray;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.amazonaws.xray.AWSXRayRecorder;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.entities.Subsegment;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author eitan.sela
+ *
+ */
+@Slf4j
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+	
+    AWSXRayRecorder xrayRecorder = AWSXRayRecorderBuilder.defaultRecorder();
+    
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(Application.class);
+		application.setWebEnvironment(false);
+		application.run(args);
+		}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.debug("run - start");
+				
+    	long timeBefore = System.currentTimeMillis();
+        
+        Subsegment subsegment = xrayRecorder.beginSubsegment("MyCustomSubsegment-01");
+		
+        log.debug("run - before sleep");
+        Thread.sleep(2000);
+        log.debug("run - after sleep");
+        
+        xrayRecorder.endSubsegment();
+        
+        log.debug("run - end - Time Took (ms): {}", (System.currentTimeMillis() - timeBefore));
+	}
+}
